ec153b3568e3090b777e826b42d1d88db3cdb31a
manish7479dlp <manish7479dlp@gmail.com>
2024-02-09 13:46:49 +0530

    add all user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,26 @@
 const express = require("express")
 const router = express.Router();
+const {verifyJWT} = require("../middlewares/auth.middlewares")
 
-const {createUser, login} = require("../controllers/user.controllers")
+const {createUser, login, updateFirstName, updateLastName, updateEmail, updatePassword} = require("../controllers/user.controllers")
 
 //create user route
 router.post("/create" , createUser);
 
 // login route
 router.post("/login" , login)
+
+//update firstName route
+router.patch("/update/first-name" , verifyJWT , updateFirstName)
+
+//update lastName route
+router.patch("/update/last-name" , verifyJWT , updateLastName)
+
+//update email route
+router.patch("/update/email" , verifyJWT , updateEmail)
+
+//update email route
+router.patch("/update/password" , verifyJWT , updatePassword)
 
 
 module.exports = router;
