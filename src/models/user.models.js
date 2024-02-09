e4c07764f5101a44001c7e57ab110c0736752c15
manish7479dlp <manish7479dlp@gmail.com>
2024-02-09 09:12:18 +0530

    create userModel

---
+++ src/models/user.models.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcryptjs = require("bcryptjs");
+
+//define user schema
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "firstName required"],
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: [true, "lastName required"],
+      trim: true,
+    },
+    userName: {
+      type: String,
+      required: [true, "userName required"],
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email-id required"],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password required"],
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//hash the password before saving the data into the database.
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  } else {
+    this.password = await bcryptjs.hash(this.password, 10);
+    next();
+  }
+});
+
+//check the password isCorrect or not.
+userSchema.method.isPasswordCorrect = async function (password) {
+  return await bcryptjs.compare(password, this.password);
+};
+
+//generate the access token
+userSchema.method.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      userName: this.userName,
+    },
+    process.env.BCRYPTJS_SECRET_KEY,
+    {
+      expiresIn: BCRYPTJS_TOKEN_EXPIRES_IN,
+    }
+  );
+};
+
+const User = mongoose.model("user" , userSchema);
+
+module.exports = User;
