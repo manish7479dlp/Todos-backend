cb035ab1648f6c3bdb640ed3affa54956ca129fa
manish7479dlp <manish7479dlp@gmail.com>
2024-02-09 11:46:58 +0530

    add dynamic database name

--- src/db/connectDb.js
+++ src/db/connectDb.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
+const {DB_NAME } = require("../constant")
 
 const connectDb = async () => {
    try {
-      const connectionInstance = await mongoose.connect(process.env.MONGO_DB_URL);
+      const connectionInstance = await mongoose.connect(process.env.MONGO_DB_URL + `/DB_NAME`);
       console.log("Database Connected sucessfully")
    } catch (error) {
       console.log("Mongo DB connection failed.", error)
