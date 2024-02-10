aa52d8bac1b91d10f7983d0ff03e6640df981e89
manish7479dlp <manish7479dlp@gmail.com>
2024-02-10 10:54:31 +0530

    add: update title routes

--- src/routes/todos.routes.js
+++ src/routes/todos.routes.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const { verifyJWT } = require("../middlewares/auth.middlewares")
-const { createTodos } = require("../controllers/todos.controllers")
+const { createTodos, updateTitle } = require("../controllers/todos.controllers")
 const router = express.Router()
 
 //create todos route
 router.post("/create" , verifyJWT , createTodos);
 
+//update todos title route
+router.patch("/update/title/:_id" , verifyJWT , updateTitle);
 
 
 module.exports = router;
