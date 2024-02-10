3952ed45636abcaf332050919f0407fa3b18ec10
manish7479dlp <manish7479dlp@gmail.com>
2024-02-10 12:45:31 +0530

    add: update task name route

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { verifyJWT } = require("../middlewares/auth.middlewares");
-const { addTask } = require("../controllers/task.controllers");
+const { addTask, updateTaskName } = require("../controllers/task.controllers");
 const router = express.Router();
 
 //add task 
 router.post("/add/:_id", verifyJWT , addTask)
+
+//update task name 
+router.patch("/update/task-name/:_id", verifyJWT , updateTaskName)
 
 module.exports = router
