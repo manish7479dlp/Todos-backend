43405d8d5ce9e0e4a993cb06fd78d5ac10d88b3e
manish7479dlp <manish7479dlp@gmail.com>
2024-02-10 11:02:47 +0530

    feat: add createdBy field in task model

--- src/models/task.models.js
+++ src/models/task.models.js
@@ -11,6 +11,10 @@ const taskSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
   { timeseries: true }
 );
