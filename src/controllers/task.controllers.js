0f94ec6ce0307deef763195dbfb5dbf58847a1ea
manish7479dlp <manish7479dlp@gmail.com>
2024-02-10 21:40:57 +0530

    fix: response type

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -8,17 +8,23 @@ const addTask = async (req, res) => {
     const todosId = req.params?._id;
 
     if (!todosId) {
-      return res.json({ status: false, message: "todos id resqured" });
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "todos id resqured"));
     }
 
     if (!taskName) {
-      return res.json({ status: false, message: "taksName resqured" });
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "taksName resqured"));
     }
 
     const todos = await Todos.findById({ _id: todosId });
 
     if (!todos) {
-      return res.json({ status: false, message: "Invalid todos id" });
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "Invalid todos id"));
     }
 
     const task = await Task.create({
@@ -28,16 +34,26 @@ const addTask = async (req, res) => {
     });
 
     if (!task) {
-      return res.json({
-        status: false,
-        message: "something went wrong in addTask controller",
-      });
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "Task is not added sucessfully"));
     }
 
-    return res.json({ status: true, message: "Task added sucessfully" });
+    return res
+      .status(200)
+      .json(new apiResponse(200, { task }, "Task added sucessfully"));
   } catch (error) {
     console.log("Error: ", error);
-    res.json({ status: false, error });
+    return res
+      .status(200)
+      .json(
+        new apiResponse(
+          200,
+          null,
+          "something went wrong in crateTask controller",
+          error
+        )
+      );
   }
 };
 
@@ -48,19 +64,45 @@ const updateTaskName = async (req, res) => {
     const taskId = req.params?._id;
 
     if (!taskId) {
-      return res.json({ status: false, message: "task id resqured" });
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "task id required"));
     }
 
     if (!taskName) {
-      return res.json({ status: false, message: "taksName resqured" });
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "taksName required"));
     }
 
-    await Task.findByIdAndUpdate(taskId, { $set: { taskName } });
+    const response = await Task.findByIdAndUpdate(
+      taskId,
+      { $set: { taskName } },
+      { new: true }
+    );
 
-    return res.json({ status: true, message: "Task Name updated sucessfully" });
+    return res
+      .status(200)
+      .json(
+        new apiResponse(
+          200,
+          { task: response },
+          "Task Name updated sucessfully"
+        )
+      );
   } catch (error) {
     console.log("Error: ", error);
     res.json({ status: false, error });
+    return res
+      .status(400)
+      .json(
+        new apiResponse(
+          400,
+          null,
+          "something went wrong in updateTaskName controller",
+          error
+        )
+      );
   }
 };
 
@@ -71,22 +113,45 @@ const updateTaskStatus = async (req, res) => {
     const status = req.body?.status;
 
     if (!taskId) {
-      return res.json({ status: false, message: "task id resqured" });
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "task id required"));
     }
 
     if (!status) {
-      return res.json({ status: false, message: "status  resqured" });
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "status field required"));
     }
 
-    await Task.findByIdAndUpdate(taskId, { $set: { status } });
+    const response = await Task.findByIdAndUpdate(
+      taskId,
+      { $set: { status } },
+      { new: true }
+    );
 
-    return res.json({
-      status: true,
-      message: "status of task updated sucessfully",
-    });
+    return res
+      .status(200)
+      .json(
+        new apiResponse(
+          200,
+          { task: response },
+          "status of task updated sucessfully"
+        )
+      );
   } catch (error) {
     console.log("Error: ", error);
-    res.json({ status: false, error });
+
+    return res
+      .status(400)
+      .json(
+        new apiResponse(
+          400,
+          null,
+          "something went wrong in updateStatus controller",
+          error
+        )
+      );
   }
 };
 
@@ -96,19 +161,27 @@ const deleteTask = async (req, res) => {
     const taskId = req?.params?._id;
 
     if (!taskId) {
-      res.json({ status: false, message: "task id required" });
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "task id required"));
     }
 
     const task = await Task.findById(taskId);
     if (!task) {
-      res.json({ status: false, message: "invalid task id." });
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "invalid task id"));
     }
 
     await Task.findByIdAndDelete(taskId);
-    res.json({ status: true, message: "Task deleted sucessfully" });
+    return res
+        .status(200)
+        .json(new apiResponse(200, null, "Task deleted sucessfully"));
   } catch (error) {
     console.log("Error: ", error);
-    res.json({ status: false, error });
+    return res
+        .status(400)
+        .json(new apiResponse(400, null, "something went wrong in deleteTask controller",error));
   }
 };
 
