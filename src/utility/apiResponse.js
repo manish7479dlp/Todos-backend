cde729bbf6286599db39fbc3fdcd7bcb41c56d9a
manish7479dlp <manish7479dlp@gmail.com>
2024-02-10 20:14:00 +0530

    feat: implement templet of response

---
+++ src/utility/apiResponse.js
@@ -0,0 +1,11 @@
+class apiResponse {
+    constructor(statusCode , data , message , error = null) {
+        this.statusCode = statusCode,
+        this.data = data,
+        this.message = message,
+        this.error = error,
+        this.status = statusCode < 400
+    }
+}
+
+module.exports = {apiResponse}
