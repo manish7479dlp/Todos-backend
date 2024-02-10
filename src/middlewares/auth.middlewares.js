e28517c505f9620bc68186ed78441b079cb1ec25
manish7479dlp <manish7479dlp@gmail.com>
2024-02-10 22:12:45 +0530

    fix: response type

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,34 +1,36 @@
 const User = require("../models/user.models");
 const jwt = require("jsonwebtoken");
+const apiResponse = require("../utility/apiResponse");
 
+const verifyJWT = async (req, res, next) => {
+  try {
+    const token = req.header("authorization")?.replace("Bearer", "");
 
-const verifyJWT = async (req , res , next) => {
-     try {
-        const token = req.header("authorization")?.replace("Bearer" , '');
+    if (!token) {
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "unauthorized User"));
+    }
 
-        
-        if(!token) {
-            res.status(404).json({statusCode: 404 , status: false , message: "unauthorized User"})
-            return;
-        }
-        
-        const decodedToken =  jwt.verify(token , process.env.JWT_SECRET_KEY)
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-        const user = await User.findById(decodedToken?._id);
+    const user = await User.findById(decodedToken?._id);
 
-        if(!user) {
-            res.status(404).json({statusCode: 404 , status: false , message: "unauthorized User"})
-            return;
-        }
+    if (!user) {
+      return res
+        .status(400)
+        .json(new apiResponse(400, null, "Invalid Credential"));
+    }
 
-        req.user = user;
-        // req.accessToken = await user.generateAccessToken();
-        next();
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Error:", error);
+    res.send(error);
+    return res
+    .status(400)
+    .json(new apiResponse(400, null, "Something went wrong in verifyJWT auth",error));
+  }
+};
 
-
-     } catch (error) {
-        console.log("Error:", error)
-     }
-}
-
-module.exports = {verifyJWT}
+module.exports = { verifyJWT };
