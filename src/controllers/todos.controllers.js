d208fb06f4acfa4308b345b6f8370b743c145f6c
manish7479dlp <manish7479dlp@gmail.com>
2024-02-10 10:54:17 +0530

    feat: implement update title functionality

--- src/controllers/todos.controllers.js
+++ src/controllers/todos.controllers.js
@@ -1,31 +1,63 @@
-const Todos = require("../models/todos.models")
-
+const Todos = require("../models/todos.models");
 
 //create todos
-const createTodos = async (req , res) => {
-    try {
-        const {title} = req.body
+const createTodos = async (req, res) => {
+  try {
+    const { title } = req.body;
 
-        if(!title) {
-            return res.json({status: false , message: "title required"})
-        }
-         
-        //create new todos document
-        const todos = await Todos.create({
-            title,
-            createdBy: req?.user?._id,
-        })
-
-        if(todos) {
-            return res.json({status: true , message: "todos created sucessfully"})
-        }
-
-        return res.json({status: false , message: "something went wrong"})
-
-    } catch (error) {
-        console.log("Error: ", error);
+    if (!title) {
+      return res.json({ status: false, message: "title required" });
     }
-}
 
+    //create new todos document
+    const todos = await Todos.create({
+      title,
+      createdBy: req?.user?._id,
+    });
 
-module.exports = {createTodos}
+    if (todos) {
+      return res.json({ status: true, message: "todos created sucessfully" });
+    }
+
+    return res.json({ status: false, message: "something went wrong" });
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+// update title
+const updateTitle = async (req, res) => {
+  try {
+    const { title } = req.body;
+    const todosId = req.params._id;
+
+    console.log(title , todosId)
+
+    if (!todosId) {
+      return res.json({ status: false, message: "todos id required" });
+    }
+
+    if (!title) {
+      return res.json({ status: false, message: "title required" });
+    }
+
+    const todos = await Todos.findById({_id: todosId});
+
+    if (!todos) {
+      return res.json({ status: false, message: "Invalid todos id" });
+    }
+
+    await Todos.findByIdAndUpdate(todosId, {
+      $set: {
+        title,
+      },
+    });
+
+    return res.json({ status: true, message: "title updated sucessfully" });
+
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+module.exports = { createTodos, updateTitle };
